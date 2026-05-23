ca9048674788d3ee4052c1e780178d4fda719372
Shaun MR <shaunmr009@gmail.com>
2026-05-23 13:27:23 +0530

    fix:padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import About from "@/components/sections/about";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen bg-[#57A4E8] px-2 py-3">
+    <div className="w-full min-h-screen bg-[#57A4E8] px-1 md:px-2 py-2 md:py-3">
       <Hero />
       <About />
       <ServicesSection />
